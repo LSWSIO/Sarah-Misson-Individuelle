348119f08d2709837cbb4b06f5151aa07b5077ca
Sarah <sarah.galichon@gmail.com>
2016-10-03 17:02:40 +0200

    Test

---
+++ AndroidStudioProjects/Galaxie/app/src/main/java/com/example/sarah/galaxie/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sarah.galaxie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Get a string resource from your app's Resources
+        String hello = getResources().getString(R.string.Hello_World);
+
+// Or supply a string resource to a method that requires a string
+        TextView textView = new TextView(this);
+        textView.setText(R.string.Hello_World);
+    }
+}
